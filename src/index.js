ec7e3fb3ae420965e2a3428ebbb1f3c744ec17d3
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-11-08 15:38:20 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -12,6 +12,15 @@ dotenv.config({
 // connectDB()
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!",err);
+})
+
 
 
 
