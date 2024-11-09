abd2834555abab1d1953c285b92dddc6c4e55476
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-11-09 23:42:34 +0530

    refreshtoken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -270,7 +270,7 @@ const logoutUser = asyncHandler(async(req,res) =>{
 const refreshAccessToken = asyncHandler(async (req,res) =>{
     const incomingRefreshtoken = req.cookies.refreshToken || req.body.refreshToken
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new ApiError(401,"unauthorized request")
     }
 
