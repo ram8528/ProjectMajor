71d39ddf2f00cb5859d52dd02bdffa9dba360df8
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-11-10 14:40:52 +0530

    Fixes bug of all Postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -267,11 +267,11 @@ const logoutUser = asyncHandler(async(req,res) =>{
 })
 
 
-const refreshAccessToken = asyncHandler(async (req,res) =>{
-    const incomingRefreshtoken = req.cookies.refreshToken || req.body.refreshToken
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if(!incomingRefreshToken){
-        throw new ApiError(401,"unauthorized request")
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "unauthorized request")
     }
 
     try {
@@ -280,14 +280,15 @@ const refreshAccessToken = asyncHandler(async (req,res) =>{
             process.env.REFRESH_TOKEN_SECRET
         )
     
-        const user = await User.findById(decodedToken?.id)
+        const user = await User.findById(decodedToken?._id)
     
-        if(!user){
-            throw new ApiError(401,"Invalid refresh token")
+        if (!user) {
+            throw new ApiError(401, "Invalid refresh token")
         }
     
-        if(incomingRefreshToken !== user?.refreshToken){
-            throw new ApiError(401,"Refresh token is expired or used")
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired or used")
+            
         }
     
         const options = {
@@ -296,22 +297,22 @@ const refreshAccessToken = asyncHandler(async (req,res) =>{
         }
     
         const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+    
         return res
         .status(200)
-        .cookie("accessToken",accessToken, options)
-        .cookie("refreshToken",newRefreshToken, options)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
         .json(
             new ApiResponse(
-                200,
+                200, 
                 {accessToken, refreshToken: newRefreshToken},
                 "Access token refreshed"
             )
         )
     } catch (error) {
-        throw new ApiError(401, error?.message || 
-            "Invalid refresh token"
-        )
+        throw new ApiError(401, error?.message || "Invalid refresh token")
     }
+
 })
 
 
